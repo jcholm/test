a50719521ccdc3f802137c48a84372a6f8f23572
jcholm <johancholmberg@hotmail.com>
2014-06-15 21:11:11 +0200

    test commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -17,7 +17,7 @@ public class Test {
      */
     public static void main(String[] args) {
         System.out.println("hejhej");
-        System.out.println("mat");
+        System.out.println("testa");
     }
     
 }
